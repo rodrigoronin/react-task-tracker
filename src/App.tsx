4e33b94503a40cbc64748c56067ceebe0fc1b90e
rodrigo ronin <liramailbox@gmail.com>
2025-03-19 02:55:16 -0300

    feat: simple form to add new tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,37 @@
-import './App.css';
+import { useState } from "react";
+import TaskForm from "./components/TaskForm";
+import "./App.css";
 
 const App: React.FC = () => {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  const addTask = (task: {
+    title: string;
+    category: string;
+    dueDate: string;
+  }) => {
+    const newTask: Task = {
+      ...task,
+      id: new Date().getTime().toString(),
+      complete: false,
+    };
+
+    setTasks([...tasks, newTask]);
+  };
 
   return (
-      <div className="App">
-        <h1>Task Tracker</h1>
-        <div className="container">
-          {/* TODO: <Tasks /> */}
-        </div>
+    <div className="App">
+      <h1>Task Tracker</h1>
+      <TaskForm onAddTask={addTask} />
+      <div className="container">
+        <p>Tasks: {tasks.length}</p>
+        <ul>
+          {tasks.map((task) => (
+            <li key={task.id}>{`${task.title} - ${task.category}`}</li>
+          ))}
+        </ul>
       </div>
+    </div>
   );
 };
 
---
+++ src/components/TaskForm.tsx
@@ -0,0 +1,54 @@
+import { useState, FormEvent } from "react";
+
+interface FormProps {
+  onAddTask: (task: {
+    title: string;
+    category: string;
+    dueDate: string;
+  }) => void;
+}
+
+const TaskForm: React.FC<FormProps> = ({ onAddTask }) => {
+  const [newTask, setNewTask] = useState({
+    title: "",
+    category: "",
+    dueDate: "",
+  });
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    if (!newTask.title || !newTask.category || !newTask.dueDate) return;
+
+    onAddTask({
+      title: newTask.title,
+      category: newTask.category,
+      dueDate: newTask.dueDate,
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={newTask.title}
+        onChange={(e) => setNewTask({ ...newTask, title: e.target.value })}
+        placeholder="Task title"
+      />
+      <input
+        type="text"
+        value={newTask.category}
+        onChange={(e) => setNewTask({ ...newTask, category: e.target.value })}
+        placeholder="Task category"
+      />
+      <input
+        type="date"
+        value={newTask.dueDate}
+        onChange={(e) => setNewTask({ ...newTask, dueDate: e.target.value })}
+        placeholder="Task title"
+      />
+      <button type="submit">Add</button>
+    </form>
+  );
+};
+
+export default TaskForm;
