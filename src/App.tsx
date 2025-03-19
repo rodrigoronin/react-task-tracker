3d041e47cb3a5a7f21bbf1f49e9f1126d32535c6
rodrigo ronin <liramailbox@gmail.com>
2025-03-19 02:14:44 -0300

    chore: base project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+const App: React.FC = () => {
+
+  return (
+      <div className="App">
+        <h1>Task Tracker</h1>
+        <div className="container">
+          {/* TODO: <Tasks /> */}
+        </div>
+      </div>
+  );
+};
+
+export default App;
